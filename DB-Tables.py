db1a36da3bd9c1022cbdc4bbc0773951a1a94911
Turgut12 <68219177+Turgut12@users.noreply.github.com>
2020-07-17 15:29:39 +0200

    Update DB-Tables.py

--- DB-Tables.py
+++ DB-Tables.py
@@ -7,14 +7,17 @@ Created on Wed Jul 15 14:34:44 2020
 
 from lxml import etree
 import DataFrame
+from psycopg2 import sql
 import psycopg2
 import sys
 import table_schema
 
+
+
 table_sizes = table_schema.table_sizes_dictionary
                  
-tree = etree.parse("part_000001.xml") #load file
-tree.xinclude()   
+tree = etree.parse("xml_files/test_2_b.xml") #load filed
+tree.xinclude()             
 
 root = tree.getroot()
 
@@ -36,8 +39,9 @@ record = cursor.fetchone()
 print("You are connected to - ", record,"\n")
 
 def create_tables():   
+    cursor.execute(table_schema.test_1_tbl_command)
+    cursor.execute(table_schema.test_2_tbl_command)
     
-    cursor.execute(table_schema.users_tbl_command)
     
 #### uncommenting the for expression below would create all the tables within the DataBase
 #    for command in table_schema.all_tbl_commands:
@@ -49,14 +53,12 @@ def add_attributes(table_name, lst_of_attributes):
         in_str = ','.join(['%s'] * len(lst_of_attributes)) # This will generate a string '%s,%s,%s...', so that the attributes can be passed
         query = "INSERT INTO {} VALUES ({})".format(table_name, in_str)
         cursor.execute(query, lst_of_attributes)
-        cursor.execute("SELECT * FROM users")
-        print(cursor.fetchall())
     except Exception:
         print("ERROR: ILLEGAL VALUE - Users Table")
         print(sys.exc_info()[1])
         
-def add_DataFrame(df):
-    table_name = root.tag
+def add_DataFrame(df, root):
+    table_name = df[0][0]
     if table_name == "user":
         table_name = "users"
     record_fields = df[0].tolist()
@@ -64,62 +66,76 @@ def add_DataFrame(df):
     record_values = df[2].tolist()
     add_record(table_name, record_fields, record_references, record_values)
 
-def reference_ID_and_subroot(reference):
-    sub_tree = etree.parse(reference)
+def reference_ID_and_subroot(file_path):
+    sub_tree = etree.parse(file_path)
     sub_root = sub_tree.getroot()
     if sub_root == "user":
         sub_root = "users"
     df = DataFrame.objectify_root(sub_root)
-    ID = df[2][0]
+    ID = df[2][1]
     return [ID, sub_root]
 
-def reference_exists(reference):
-    ID_and_subroot = reference_ID_and_subroot(reference)
+def reference_exists(file_path):
+    ID_and_subroot = reference_ID_and_subroot(file_path)
     ID = ID_and_subroot[0]
     sub_root = ID_and_subroot[1]
-    return cursor.execute("select exists(select 1 from %s where ID = %s)", [sub_root.tag, ID])
+    cursor.execute(sql.SQL("select exists(select 1 from {} where ID = (%s))").format(sql.Identifier(sub_root.tag)), [ID])
+    boolean = cursor.fetchall()[0][0] #fetchall returns a list of tuples. We have to dereference twice to get the boolean
+    return boolean
      
 def add_record(table_name, record_fields, record_references, record_values):  
     def get_list_of_attributes():
         lst_of_attributes = []
-        indx = 0
-        triplets = zip(record_fields, record_references, record_values)
-        for field, reference, value in triplets:
-            indx = indx + 1
+        #indx = 0 (IGNORE INDX VAR FOR NOW)
+        for field, attribute, value in zip(record_fields, record_references, record_values):
+            #indx = indx + 1
             
-            if value == "Xin":#this branch handles Xincluded records, by recursively calling add_record
-                add_record(field.split('_')[0], record_fields[indx:], record_references[indx:], record_values[indx:])
-                attributes_to_skip = table_sizes[field.split('_')[0]]
-                while(attributes_to_skip): #the recursive call will take care of adding the Xincluded record, the parent has to skip the attributes
-                    next(triplets)
-                    indx = indx + 1
-                    attributes_to_skip = attributes_to_skip - 1
-                    
-            elif value:
+            if value:
                 lst_of_attributes.append(value)
                 if (len(lst_of_attributes) == table_sizes[table_name]):
                     return lst_of_attributes
                 
-            elif reference:
-                if (not reference_exists(reference)): #check if the reference already exists in table
-                      sub_tree = etree.parse(reference)
+            elif "refid" in attribute:
+                file_path = attribute["refid"]
+                if (not reference_exists(file_path)): #check if the reference already exists in table
+                      sub_tree = etree.parse(file_path)
                       sub_tree.xinclude()
                       sub_root = sub_tree.getroot()
                       df = DataFrame.objectify_root(sub_root)
-                      add_DataFrame(df) #if add the Data within the file being referred to
-                lst_of_attributes.append(reference_ID_and_subroot(reference)[0]) #otherwise, add the ID
+                      add_DataFrame(df, sub_root) #if so, add the data within the file being referred to
+                lst_of_attributes.append(reference_ID_and_subroot(file_path)[0]) #add the ID
+                if (len(lst_of_attributes) == table_sizes[table_name]):
+                    return lst_of_attributes
+                
+#            elif "fileName" in attribute: #branch for Xincluded records
+#                add_record(field, record_fields[indx:], record_references[indx:], record_values[indx:]) #add the Xincluded record
+#                attribute_to_skip = table_sizes[record_fields[0]]
+#                while(attribute_to_skip): #the recursive call will take care of adding the Xincluded record, the parent has to skip the attributes
+#                    print("skipping attributes")
+#                    next(triplets)
+#                    indx = indx + 1
+#                    attribute_to_skip = attribute_to_skip - 1          
                     
     l = get_list_of_attributes()
-    print(l)
     add_attributes(table_name, l)
     return True
         
         
         
-#create_tables()
-#add_DataFrame(df)
-#cursor.execute("DROP TABLE Part CASCADE")
-#cursor.execute("DROP TABLE users CASCADE")
+create_tables()
+add_DataFrame(df, root)
+
+print('\n')
+print("TEST_1 contents!!!")
+cursor.execute("SELECT * FROM test_1")
+print(cursor.fetchall())
+print('\n')
+print("TEST_2 contents!!!")
+cursor.execute("SELECT * FROM test_2")
+print(cursor.fetchall())
+
+#cursor.execute("DROP TABLE test_1 CASCADE")
+#cursor.execute("DROP TABLE test_2 CASCADE")
 connection.commit()
     
 
